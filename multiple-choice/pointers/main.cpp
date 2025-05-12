cde85468e5efca1550f793c9efdfed82f22008e5
secretlycharles <imcharlesmendez@gmail.com>
2025-05-12 01:34:44 -0700

    added ptr value reassignment

--- multiple-choice/pointers/main.cpp
+++ multiple-choice/pointers/main.cpp
@@ -15,9 +15,13 @@ int main() {
     int c = *b;
     cout << "C: " << c << endl;
 
+    // We can also define a value
+    *b = 1337;
+    cout << "B: " << *b << endl;
+
     // Example output
     // A: 42069 (Value of A)
     // B: 0x7ffeeed1acdc (Memory Address Value of A)
-    // C: 42069 (Deferences A Pointer to get Value)
-
+    // C: 42069 (Deferences A Pointer to get Value at memory address)
+    // B: 1337 (Setting the value of the referencing pointer value)
 };
