201d4adfd0af88583562bbafec5bb3c0ecc27bd5
secretlycharles <imcharlesmendez@gmail.com>
2025-05-12 01:32:55 -0700

    pointers simplified

---
+++ multiple-choice/pointers/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // This is an integer
+    int a = 42069;
+    cout << "A: " << a << endl;
+
+    // This is referencing a pointer value
+    int *b = &a;
+    cout << "B: " << b << endl;
+
+    // This is dereferencing a pointer
+    int c = *b;
+    cout << "C: " << c << endl;
+
+    // Example output
+    // A: 42069 (Value of A)
+    // B: 0x7ffeeed1acdc (Memory Address Value of A)
+    // C: 42069 (Deferences A Pointer to get Value)
+
+};
